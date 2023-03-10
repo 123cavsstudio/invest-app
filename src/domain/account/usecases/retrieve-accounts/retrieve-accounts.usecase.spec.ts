1f0fd855e90cdb70bd0d746cb628f77f58b07bf2
remcalmels <github@remi.calmels.eu>
2023-03-10 17:49:53 +0100

    adding jest-mock-extended library + update TU with mock + adding verbose option for testing

--- src/domain/account/usecases/retrieve-accounts/retrieve-accounts.usecase.spec.ts
+++ src/domain/account/usecases/retrieve-accounts/retrieve-accounts.usecase.spec.ts
@@ -2,6 +2,7 @@ import { Account } from "@domain/account/entitites/account";
 import { AccountApi } from "@domain/account/ports/account.api";
 import { RetrieveAccountsUseCase } from "@domain/account/usecases/retrieve-accounts/retrieve-accounts.usecase";
 import { Observable, of } from "rxjs";
+import { mock } from "jest-mock-extended";
 
 describe("RetrieveAccountsUseCase", () => {
 
@@ -11,10 +12,10 @@ describe("RetrieveAccountsUseCase", () => {
       { id: "1", label: "account_1", type: "type_1" },
       { id: "2", label: "account_2", type: "type_2" }
     ];
-    const api: AccountApi = {
-      getAccounts: () => of(expectedAccounts)
-    };
-    const useCase = new RetrieveAccountsUseCase(api);
+    const mockApi = mock<AccountApi>();
+    mockApi.getAccounts.mockReturnValue(of(expectedAccounts));
+
+    const useCase = new RetrieveAccountsUseCase(mockApi);
 
     // When
     useCase.execute().subscribe((accounts: Account[]) => {
